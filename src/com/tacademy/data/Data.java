ef72b0ca6f9731ae2f3b91ae4a8495a5d8722a26
minggry <minggry@gmail.com>
2017-07-21 09:32:46 +0900

    age add

--- src/com/tacademy/data/Data.java
+++ src/com/tacademy/data/Data.java
@@ -3,6 +3,7 @@ package com.tacademy.data;
 public class Data {
 
 	private String name;
+	private int age;
 	public Data() {
 		// TODO Auto-generated constructor stub
 	}
@@ -12,9 +13,16 @@ public class Data {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	@Override
 	public String toString() {
-		return "Data [name=" + name + "]";
+		return "Data [name=" + name + ", age=" + age + "]";
 	}
 	
 	
